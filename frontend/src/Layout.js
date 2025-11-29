8c1af1c04b7443fdae2aed6c44cb621fd2045574
sahiltejwani <tejwanisahil73@gmail.com>
2025-11-30 01:46:00 +0530

    Updated routes in frontend

--- frontend/src/Layout.js
+++ frontend/src/Layout.js
@@ -13,8 +13,6 @@ import Farmer from './screens/Farmer/FarmerScreen';
 import Create from './screens/Create/CreateScreen';
 import Supplier from './screens/Supplier/SupplierScreen';
 import ProfileScreen from './screens/Profile/ProfileScreen';
-import DashboardScreen from './screens/Dashboard/DashboardScreen';
-//import AdminProfileScreen from './screens/Dashboard/ProfileScreen';
 import Farmer_ProductSeed from './screens/Farmer_ProductSeed/Farmer_ProductSeedScreen';
 import SeedProductScreen from './screens/Product_Seed/SeedProductScreen';
 import Farmer_LendScreen from './screens/Farmer_LendMachine/Farmer_LendScreen';
@@ -24,18 +22,6 @@ import ConsumerProductDetailScreen from './screens/Product_Consumer/ConsumerProd
 import ShippingScreen from './screens/Shipping/ShippingScreen';
 import OrderScreen from './screens/Order/OrderScreen';
 import Cart from './screens/Cart/Cart';
-
-// import UserListScreen from './screens/Dashboard/UserListScreen';
-// import UserEditScreen from './screens/UserEdit/UserEditScreen';
-// import ProductListScreen from './screens/Dashboard/ProductListScreen'
-// import SeedListEdit from './screens/ProductListEdit/SeedListEdit/SeedListEdit'
-// import MachineListEdit from './screens/ProductListEdit/MachineListEdit/MachineListEdit'
-// import ConsumerListEdit from './screens/ProductListEdit/ConsumerListEdit/ConsumerListEdit'
-//import SupplierProductEdit from './components/SupplierProductEdit/SupplierProductEdit'
-// import OrderListScreen from './screens/Dashboard/OrderListScreen'
-// import HarvestScreen from './screens/Dashboard/HarvestScreen';
-// import FarmerProduct from './screens/FarmerProducts/FarmerProducts';
-// import MapScreen from './screens/Dashboard/MapScreen';
 
 const Layout = () => {
     return (
@@ -48,7 +34,6 @@ const Layout = () => {
                     <Route exact path="/create" component={Create} />
                     <Route exact path="/consumer" component={Consumer} />
                     <Route exact path="/supplier" component={Supplier} />
-                    {/* <Route exact path="/farmers/sellMeterial" component={LoginComponent} /> */}
                     <Route exact path="/login" component={LoginComponent} />
                     <Route exact path="/register" component={Register} />
                     <Route exact path="/profile" component={ProfileScreen} />
@@ -57,20 +42,6 @@ const Layout = () => {
                     <Route exact path="/placeorder" component={PlaceOrder} />
                     <Route exact path="/cart/:id?" component={Cart} />
                     <Route path="/order/:id" component={OrderScreen} />
-
-                    {/* <Route exact path="/admin/userList" component={UserListScreen} />
-                    <Route exact path="/admin/dashboard" component={DashboardScreen} />
-                    <Route exact path="/admin/profile" component={AdminProfileScreen} />
-                    <Route exact path="/admin/productlist" component={ProductListScreen} />
-                    <Route exact path="/admin/orderlist" component={OrderListScreen} />
-                    <Route exact path="/admin/supplierproducts" component={HarvestScreen} />
-                    <Route exact path="/admin/map" component={MapScreen} />
-                    <Route exact path="/admin/productlist/seed/:id/edit" component={SeedListEdit} />
-                    <Route exact path="/admin/productlist/machine/:id/edit" component={MachineListEdit} />
-                    <Route exact path="/admin/productlist/consumer/:id/edit" component={ConsumerListEdit} />
-                    <Route exact path="/admin/user/:id/edit" component={UserEditScreen} /> */}
-                    {/* <Route exact path="/supplierproducts/:id/review" component={FarmerProduct} />
-                    <Route exact path="/supplierproducts/:id/edit" component={SupplierProductEdit} /> */}
 
                     <Route exact path="/farmers/purchaseSeeds" component={Farmer_ProductSeed} />
                     <Route exact path="/farmers/purchaseSeeds/:id" component={SeedProductScreen} />
